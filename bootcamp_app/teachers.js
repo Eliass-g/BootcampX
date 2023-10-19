e05be84b7d71d5d73bb6b42cbac4d721a2dc1f4f
Eliass-g <eliass.ghauss@gmail.com>
2023-10-19 07:25:56 -0400

    SQL injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,4 @@
 const { Pool } = require('pg');
-const args = process.argv;
 
 const pool = new Pool({
   user: 'vagrant',
@@ -8,15 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+
+const values = [`${cohortName}`];
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
